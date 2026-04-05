1d563576beb75cd17aeae594f2cfddbd3cbb2742
bijirin <tsutrisn@gmail.com>
2026-04-05 20:12:59 +0700

    Implement cinema visit functionality with customer and cinema hall management

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-# write your imports here
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customers_list = []
+
+    for customer in customers:
+        customers_list.append(Customer(customer["name"], customer["food"]))
+
+    for customer in customers_list:
+        CinemaBar.sell_product(customer, customer.food)
+
+    cinema_hall = CinemaHall(hall_number)
+    cinema_hall.movie_session(movie, customers_list, Cleaner(cleaner))
